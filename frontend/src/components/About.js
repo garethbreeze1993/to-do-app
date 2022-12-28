8a057f127291682dde500a6c758140c3dfa8836e
Gareth Breeze <gareth.breeze@hypaship.com>
2022-12-28 19:13:40 +0000

    Fix bug on About screen with Navbar component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,12 +1,35 @@
 import Nav from "./Navbar"
+import React from "react";
+import base_api from "../base_api";
+
 
 function About() {
-  return (
-    <main>
-        <Nav />
-        <p>About TBC</p>
-    </main>
-  );
+    const [loggedIn, setLoggedIn] = React.useState(false);
+    const [userEmail, setUserEmail] = React.useState('');
+    const userToken = localStorage.getItem('userToken');
+
+    React.useEffect(() => {
+    if(userToken){
+    base_api.get(`/api/v1/users/get_user_data`)
+        .then((response) => {
+            setLoggedIn(true)
+            setUserEmail(response.data.email)
+          })
+        .catch(err => {
+          setLoggedIn(false)
+        })
+    }
+  }, [userToken])
+
+    return (
+        <main>
+            <Nav
+                userEmail={userEmail}
+                loggedIn={loggedIn}
+            />
+            <p>About TBC</p>
+        </main>
+    );
 }
 
 export default About;
