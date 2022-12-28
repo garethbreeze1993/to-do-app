11b084be3a0d86f4d675e19fa117c190125138bd
Gareth Breeze <gareth.breeze@hypaship.com>
2022-12-28 18:14:03 +0000

    Fix unused import so project can build inside CI pipeline

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import base_api from "../base_api";
 
 
 function NavComponent(props) {
